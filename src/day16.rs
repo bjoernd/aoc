30ebc379b1b74540a17cd0bb4ec2cbdccc7ca686
Bjoern Doebel <bjoern.doebel@proton.me>
2024-12-23 23:12:02 +0100

    Day 16, part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,7 +1,10 @@
 use itertools::Itertools;
 use std::cmp::Ordering;
 use std::hash::Hash;
-use std::{collections::HashMap, collections::HashSet};
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::BinaryHeap;
+use std::usize::MAX;
 
 use crate::{DaySolution, FromInput};
 
@@ -80,7 +83,10 @@ impl DaySolution for Day16 {
 
         // println!("Searching path from {},{} to {},{}", start_l, start_c, end_l, end_c);
 
+        // Dijkstra algo: unvisited nodes
         let mut unvisited = HashSet::<Node>::new();
+
+        // this tracks all the intermediate results for the Dijkstra implementation below
         let mut results = vec![];
 
         unvisited.insert(Node {
@@ -91,7 +97,12 @@ impl DaySolution for Day16 {
         });
 
         for l in 0..self.map.len() {
+            let mut resline = vec![];
             for c in 0..self.map[0].len() {
+                resline.push(Node{line: l, col: c, cost: MAX, direction: Direction::Unknown});
+
+                if c == start_c && l == start_l { continue; }
+
                 if self.map[l][c] != '#' {
                     unvisited.insert(Node {
                         line: l,
@@ -99,11 +110,15 @@ impl DaySolution for Day16 {
                         cost: usize::MAX,
                         direction: Direction::Unknown,
                     });
-                    // println!("{} rc {}", unvisited.len(), rc);
+                    // println!("{}", unvisited.len());
                 }
             }
+            results.push(resline);
         }
 
+        results[start_l][start_c].cost = 0;
+
+        /* Full Dijkstra -- compute all best values until we have no reachable fields anymore */
         loop {
             let current_node = unvisited.iter().min().cloned();
 
@@ -116,10 +131,6 @@ impl DaySolution for Day16 {
                 Some(node) => {
                     if node.cost == usize::MAX {
                         break;
-                    }
-
-                    if node.line == end_l && node.col == end_c {
-                        results.push(node);
                     }
                 }
             }
@@ -239,11 +250,170 @@ impl DaySolution for Day16 {
             }
 
             unvisited.remove(&current_node);
+
+            results[current_node.line][current_node.col] = current_node;
         }
 
         // println!("{:?}", results);
+        // let mut map = self.map.clone();
 
-        results[0].cost.to_string()
+        // let mut il = end_l;
+        // let mut ic = end_c;
+        // loop {
+        //     if il == start_l && ic == start_c { break; }
+
+        //     match results[il][ic].direction {
+        //         Direction::Left => {
+        //             map[il][ic] = '<';
+        //             ic += 1;
+        //         },
+        //         Direction::Right => {
+        //             map[il][ic] = '>';
+        //             ic -= 1;
+        //         },
+        //         Direction::Up => {
+        //             map[il][ic] = '^';
+        //             il += 1;
+        //         },
+        //         Direction::Down => {
+        //             map[il][ic] = 'v';
+        //             il -= 1;
+        //         },
+        //         _ => {},
+        //     }
+        // }
+
+        // for i in 0..map.len() {
+        //     for j in 0..map[0].len() {
+        //         print!("{}", map[i][j]);
+        //     }
+
+        //     print!("    ");
+
+        //     for j in 0..results[0].len() {
+        //         if results[i][j].cost == MAX {
+        //             print!("..... ");
+        //         } else {
+        //             print!("{:5} ", results[i][j].cost);
+        //         }
+        //     }
+
+        //     println!();
+        // }
+
+        /* Part 2: We start at the end. We move backwards based on what we found.
+           This gives us the actual shortest route found.
+
+           For alternative routes, we need to be aware of "gaps". Example excerpt
+           from a map:
+
+              A    B    C    D    E
+            0 3008 4009 3010 4011 4012 ->
+            1 3007 #### 3009 #### ####
+            2 3006 #### 3008 #### ####
+         -> 3 2005 2006 2007 #### ####
+
+            Starting at the bottom left (A3), there are two paths with identical
+            cost to the top right (E0). The paths split at A3 and meet again at C0.
+            The second path however found a lower cost trip to C0 and thus overwrote
+            C0's cost with the lower value, but then turns to D0 and at that point
+            gets the same cost as the first path which would have gone straight up from
+            A3 to A0 and then right.
+
+            If we unroll paths backwards, this means we have to check straight lines
+            for "gaps" where a second path with a seemingly lower cost comes in. We then
+            need to check if going in a straight line we'd end up with a similarly optimal
+            path in any of the other directions and then pursue these as well.
+         */
+
+        let mut to_visit = BinaryHeap::<(usize, usize)>::new();
+        let mut visited2 = HashSet::<(usize, usize)>::new();
+        let mut path_nodes = HashSet::<(usize, usize)>::new();
+
+        to_visit.push((end_l, end_c));
+
+        while !to_visit.is_empty() {
+            let (line, col) = to_visit.pop().unwrap();
+            let my_cost = results[line][col].cost;
+
+            if self.map[line][col] == '#' {
+                continue;
+            }
+
+            if visited2.contains(&(line, col)) {
+                continue;
+            }
+
+            visited2.insert((line, col));
+
+            /* we are definitely a path node ourselves */
+            path_nodes.insert((line, col));
+            
+            let (left_l, left_c) = (line, col-1);
+            let (right_l, right_c) = (line, col+1);
+            let (down_l, down_c) = (line+1, col);
+            let (up_l, up_c) = (line-1, col);
+
+            macro_rules!  check_and_push {
+                ($dir:expr, $offset:expr) => {
+                    if my_cost > $offset + 1 &&
+                        results[$dir.0][$dir.1].cost == my_cost - 1 - $offset {
+                        to_visit.push(($dir.0, $dir.1));
+                    }
+                    if my_cost > 1001 + $offset && 
+                        results[$dir.0][$dir.1].cost == my_cost - 1001 - $offset {
+                        to_visit.push(($dir.0, $dir.1));
+                    }
+                };
+            }
+            // println!("{} {}", line, col);
+
+            /* a direct predecessor has a cost of either current - 1 (if we moved
+               in a straight line) or current - 10001 (if we had to turn)
+             */
+            if self.map[left_l][left_c] != '#' && my_cost > results[left_l][left_c].cost {
+                check_and_push!((left_l, left_c), 0);
+                check_and_push!((left_l-1, left_c), 1);
+                check_and_push!((left_l+1, left_c), 1);
+                check_and_push!((left_l, left_c-1), 1);
+            }
+
+            if self.map[right_l][right_c] != '#' && my_cost > results[right_l][right_c].cost { 
+                check_and_push!((right_l, right_c), 0);
+                check_and_push!((right_l-1, right_c), 1);
+                check_and_push!((right_l+1, right_c), 1);
+                check_and_push!((right_l, right_c+1), 1);
+            }
+
+            if self.map[up_l][up_c] != '#' && my_cost > results[up_l][up_c].cost {
+                check_and_push!((up_l, up_c), 0);
+                check_and_push!((up_l-1, up_c), 1);
+                check_and_push!((up_l, up_c-1), 1);
+                check_and_push!((up_l, up_c+1), 1);
+            }
+
+            if self.map[down_l][down_c] != '#' && my_cost > results[down_l][down_c].cost {
+                check_and_push!((down_l, down_c), 0);
+                check_and_push!((down_l+1, down_c), 1);
+                check_and_push!((down_l, down_c-1), 1);
+                check_and_push!((down_l, down_c+1), 1);
+            }
+        }
+
+        // for i in 0..map.len() {
+        //     for j in 0..map[0].len() {
+        //         if path_nodes.contains(&(i,j)) {
+        //             print!("O");
+        //         } else {
+        //             print!("{}", self.map[i][j]);
+        //         }
+        //     }
+        //     println!();
+        // }
+
+        println!("{}", path_nodes.len()+1);
+
+        results[end_l][end_c].cost.to_string()
     }
 
     fn part_two(&self) -> String {
